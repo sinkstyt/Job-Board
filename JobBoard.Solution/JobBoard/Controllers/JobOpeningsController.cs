7068983326a1b992e910173f29f8dc163f5f7221
Cat-Denton <willwdenton@gmail.com>
2021-04-27 19:25:00 -0700

    create routes for JobOpenings
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

---
+++ JobBoard.Solution/JobBoard/Controllers/JobOpeningsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Models;
+using System.Collections.Generic;
+
+namespace JobBoard.Controllers
+{
+  public class JobOpeningsController : Controller
+  {
+    [HttpGet("/jobopenings")]
+    public ActionResult Index()
+    {
+      List<JobOpening> allJobOpenings = JobOpening.GetAll();
+      return View(allJobOpenings);
+    }
+    [HttpGet("/jobopenings/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/jobopenings")]
+    public ActionResult Create(string title, string description, string name, string email, string phone)
+    {
+      JobOpening myJobOpening = new JobOpening(title, description, name, phone, email);
+      return RedirectToAction("Index");
+    }
+  }
+}
